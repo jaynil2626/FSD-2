87cb7de342b0271ab23fc3356890062f8e0e4d81
Jaynil_26 <ompatell2006@gmail.com>
2025-03-18 04:49:56 +0000

    18 march

---
+++ node_js/2file_sys.js
@@ -0,0 +1,22 @@
+var fs = require("fs");
+
+//fs.mkdirSync("node");
+
+fs.writeFileSync("node/rw.txt","Hello world");
+
+fs.appendFileSync("node/rw.txt","\n how are you all ?");
+
+data = fs.readFileSync("node/rw.txt")
+console.log(data)   // returns encoded buffer code format
+
+
+data = fs.readFileSync("node/rw.txt","utf-8")
+console.log(data)
+// OR
+data = fs.readFileSync("node/rw.txt")
+console.log(data.toString())
+
+
+fs.renameSync("node/rw.txt","node/write.txt")
+fs.writeFileSync("node/write.txt","test");
+fs.unlinkSync("node/write.txt");
