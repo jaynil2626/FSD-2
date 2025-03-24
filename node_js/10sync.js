688c6dce3f817f5ea8d845b9e9e2d80cdc8bd297
Jaynil_26 <ompatell2006@gmail.com>
2025-03-24 03:51:35 +0000

    24 march

--- node_js/10sync.js
+++ node_js/10sync.js
@@ -1,3 +1,8 @@
+// Write a Node.js script that asynchronously writes data to a file named 'test1.js'.
+// If no error occurs during the writing process, the script should then append additional data to the same file. 
+// Finally, it should read the content of the file, including the newly written and appended data, and display it in the console.
+
+
 var fs = require('fs');
 fs.writeFile('test1.txt', 'hello', (e)=> {
     if (e) { console.log(e);}
