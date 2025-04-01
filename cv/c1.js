7144fbdf8e21e3070b8731c559746a81c29dad20
Jaynil_26 <ompatell2006@gmail.com>
2025-04-01 05:00:13 +0000

    1 april

---
+++ cv/c1.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+const log = console.log;
+log("LJU");
+log("Hello" + chalk.bgCyan("World"));
+
+console.log(chalk.blue.underline.bgYellow("Testing") + chalk.red.bold.italic.underline.bgWhite("test"))
